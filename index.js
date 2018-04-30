d31920d7433ff121dd10c716f28dc66a51c1d18d
John Kosmicke <kozjob@yahoo.com>
2018-04-29 21:54:53 -0700

    update

--- index.js
+++ index.js
@@ -4,29 +4,37 @@ const Card = (props) => {
 
   return (
     <div style={{ margin:'1em' }}>
-      <img width="100" src="https://avatars1.githubusercontent.com/u/1393010?v=4" />
+      <img width="100" src={props.avatar_url} />
       <div style={{ display: 'inline-block', marginLeft: 10 }}>
-        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>john kosmicke</div>
-        <div>tetris</div>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>{ props.name }</div>
+        <div>{ props.company }</div>
       </div>
     </div>
   );
 }
 
 
-class App extends React.Component {
+let data = [
+  {
+    name: 'john kosmicke',
+    company: 'tetris', 
+    avatar_url: 'https://avatars1.githubusercontent.com/u/1393010?v=4'
+  }
+];
+
+class CardList extends React.Component {
 
   render() {
     return (
       <div>
-        <Card />
+        { this.props.cards.map( card => <Card {...card} />) }
       </div>
     );
   };
 }
 
 ReactDOM.render(
-  <App />,
+  <CardList cards={data} />,
   document.getElementById('root')
 );
 
