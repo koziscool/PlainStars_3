c96179831460a5b18626df026d0c0ed06783435c
John Kosmicke <kozjob@yahoo.com>
2018-04-29 21:46:29 -0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 
-class Card extends React.Component {
+const Card = (props) => {
 
-  render() {
-    return (
-      <div>
-        tetris
+  return (
+    <div style={{ margin:'1em' }}>
+      <img width="100" src="https://avatars1.githubusercontent.com/u/1393010?v=4" />
+      <div style={{ display: 'inline-block', marginLeft: 10 }}>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>john kosmicke</div>
+        <div>tetris</div>
       </div>
-    );
-  };
+    </div>
+  );
 }
-
-
 
 
 class App extends React.Component {
