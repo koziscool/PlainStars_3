e434026a82e4574df68c20518bf04b65f02610d0
John Kosmicke <kozjob@yahoo.com>
2018-04-30 18:41:31 -0700

    update

--- index.js
+++ index.js
@@ -1,37 +1,87 @@
 
 
+const allNumbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
 const Stars = (props) => {
+
+  let stars = [];
+  for( var i = 0; i < props.numberOfStars ; i++ ) {
+    stars.push( <i key={i} className="fa fa-star"></i>);
+  }
+    
   return (
-    <div>
-      ......
+    <div className="col-5">
+      { stars }
     </div>
   );
 };
 
 const Button = (props) => {
   return (
-    <div>
-      ......
+    <div className="col-2 btn">
+      <button>=</button>
     </div>
   );
 };
 
 const Answer = (props) => {
   return (
-    <div>
-      ......
+    <div className="col-5">
+      { props.selectedNumbers.map( ( number, i)=>
+        <span key={i}>{number}</span> ) }
     </div>
   );
 };
 
+const Numbers = (props) => {
+  
+  const numberClassName = (number) => {
+    if( props.selectedNumbers.indexOf(number) >= 0 ) return 'selected';
+  }
+  return (
+    <div className="card text-center">
+      <div>
+        { allNumbers.map( (number, i)=>
+          <span key={i} 
+                  className={numberClassName(number)}
+                  onClick={() => props.selectNumber(number) }>
+            {number}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
 class Game extends React.Component {
+
+  state = { 
+    selectedNumbers: [  ], 
+    numberOfStars: 1 + Math.floor( Math.random() * 9 ),
+  };
+
+  selectNumber = (clickedNumber) => {
+    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0 ) return;
+    this.setState( prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat( clickedNumber ),
+    }));
+  };
+
+
   render() {
     return (
-      <div>
+      <div className="container">
           <h3>Play Nine</h3>
-          <Stars />
-          <Button />
-          <Answer />
+          <hr />
+          <div className="row">
+            <Stars numberOfStars={this.state.numberOfStars} />
+            <Button />
+            <Answer selectedNumbers= {this.state.selectedNumbers} />
+          </div>
+          <br />
+          <Numbers selectedNumbers= {this.state.selectedNumbers} 
+                        selectNumber={this.selectNumber} />
       </div>
     );
   };
